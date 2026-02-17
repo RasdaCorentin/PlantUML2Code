0c332f518c2bd833e4d85a753ac543b0a1ce41c1
Benoit <benoit.dijoux@esiroi.re>
2026-02-17 09:54:04 +0400

    Ajout du diagramme Java

--- programme/src/specialisation_cpp/type_fichier_cpp.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class TypeFichierCpp(Enum):
-    HEADER = "HEADER"
-    SOURCE = "SOURCE"
--- programme/src/specialisation_cpp/type_relation_cpp.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class TypeRelationCpp(Enum):
-    ASSOCIATION = "ASSOCIATION"
-    DEPENDANCE = "DEPENDANCE"
-    AGREGATION = "AGREGATION"
